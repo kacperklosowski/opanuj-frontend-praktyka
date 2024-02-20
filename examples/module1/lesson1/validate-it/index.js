c0b6729daf6ba582b609bfd7f22d44395ca110c1
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-02-20 22:27:01 +0100

    refactor: make logic easier to read

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,32 +1,50 @@
-function validator() {
-  const input = document.getElementById('input');
-  const button = document.getElementById('button');
-  const button2 = document.getElementById('button2');
-  const result = document.getElementById('result');
+const VALIDATION_MESSAGES = {
+  ERROR: 'Invalid number. Please enter a number between 0 and 100',
+  SUCCESS: 'The number is valid'
+};
 
-  button.addEventListener('click', () => {
-    const ERROR_MESSAGE = 'Invalid number. Please enter a number between 0 and 100';
-    const SUCCESS_MESSAGE = 'Valid number. Thank you!';
+function validate(number) {
+  const validators = [
+    (number) => number > 0,
+    (number) => number < 100,
+    (number) => number % 2 === 0,
+    (number) => Number.isInteger(number)
+  ];
 
-    const inputValue = +input.value;
+  return validators.every(validator => validator(number));
+}
 
-    if(!inputValue) {
-      result.innerHTML = ERROR_MESSAGE;
+function handleValidationButtonClick(value, validationResult) {
+  const isValid = validate(value);
+
+  isValid ? validationResult.innerHTML = VALIDATION_MESSAGES.SUCCESS : validationResult.innerHTML = VALIDATION_MESSAGES.ERROR;
+}
+
+function handleClearButtonClick(validationInput, validationResult) {
+  validationInput.value = '';
+  validationResult.innerHTML = '';
+}
+
+function main() {
+  const validationInput = document.getElementById('validationInput');
+  const validationButtons = document.getElementById('validationButtons');
+  const validationResult = document.getElementById('validationResult');
+
+  validationButtons.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') { // listen for button clicks only
       return;
     }
 
-    if (inputValue > 0 && inputValue < 100 && inputValue % 2 === 0) {
-      result.innerHTML = SUCCESS_MESSAGE;
-      return;
+    const isValidationButton = event.target.id === 'validationButton';
+    const isClearButton = event.target.id === 'clearButton';
+    const inputValue = +validationInput.value;
+
+    if (isValidationButton) {
+      handleValidationButtonClick(inputValue, validationResult);
+    } else if (isClearButton) {
+      handleClearButtonClick(validationInput, validationResult);
     }
-
-    result.innerHTML = ERROR_MESSAGE;
-  });
-
-  button2.addEventListener('click', () => {
-    input.value = '';
-    result.innerHTML = '';
   });
 }
 
-validator();
+main();
