4c07fe44e2d3f08f8269ad8c194a848cfb16192a
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-03-09 14:56:05 +0100

    module2-lesson1-task1 (#4)

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -16,4 +16,24 @@ describe('Form validation', () => {
         const errors = formValidator('John', 'Doe', -1);
         expect(errors).toContain('Age must be a positive number');
     });
+
+    test('should return no errors if all fields are valid', () => {
+        const errors = formValidator('John', 'Doe', 30);
+        expect(errors.length).toBe(0);
+    });
+
+    test('should return an error if first name is too short', () => {
+        const errors = formValidator('J', 'Doe', 30);
+        expect(errors).toContain('First name must be at least 2 characters long');
+    });
+
+    test('should return an error if last name is too short', () => {
+        const errors = formValidator('John', 'D', 30);
+        expect(errors).toContain('Last name must be at least 2 characters long');
+    });
+
+    test('should throw an error if age is not a number', () => {
+        // @ts-ignore | so we can test the error
+        expect(() => formValidator('John', 'Doe', '7')).toThrow('Age must be a number');
+    });
 });
--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -9,12 +9,24 @@ export function formValidator(
     errors.push('First name is required');
   }
 
+  if (firstName && firstName.length < 2) {
+    errors.push('First name must be at least 2 characters long');
+  }
+
   if (!lastName) {
     errors.push('Last name is required');
   }
 
+  if (lastName && lastName.length < 2) {
+    errors.push('Last name must be at least 2 characters long');
+  }
+
   if (age < 0) {
     errors.push('Age must be a positive number');
+  }
+
+  if (typeof age !== 'number' || isNaN(age)) {
+    throw new Error('Age must be a number');
   }
 
   return errors;
