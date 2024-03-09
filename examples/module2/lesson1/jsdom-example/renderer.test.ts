d95d03d9cc43ed870d7ebba18c2874b2787d0a5f
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-03-09 17:06:05 +0100

    module2-lesson1-task2 (#5)

--- examples/module2/lesson1/jsdom-example/renderer.test.ts
+++ examples/module2/lesson1/jsdom-example/renderer.test.ts
@@ -10,12 +10,18 @@ const users: User[] = [
 ];
 
 describe('User renderer', () => {
+  const adminUsers = users.filter((user) => user.role === 'admin');
+  const nonAdminUsers = users.filter((user) => user.role === 'user');
+
   test('should render all users if admin is rendering the list', () => {
     localStorage.setItem('userRole', 'admin');
 
     const container = document.createElement('div');
     renderItems(container, users);
-    expect(Array.from(container.querySelectorAll('li'))).toHaveLength(3);
+
+    const userItems = Array.from(container.querySelectorAll('li'));
+
+    expect(userItems).toHaveLength(users.length);
   });
 
   test('should render only regular users if non-admin is rendering the list', () => {
@@ -23,6 +29,44 @@ describe('User renderer', () => {
 
     const container = document.createElement('div');
     renderItems(container, users);
-    expect(Array.from(container.querySelectorAll('li'))).toHaveLength(2);
+
+    const userItems = Array.from(container.querySelectorAll('li'));
+
+    expect(userItems).toHaveLength(nonAdminUsers.length);
+  });
+
+  test('should render admin label for admin users', () => {
+    localStorage.setItem('userRole', 'admin');
+
+    const container = document.createElement('div');
+    renderItems(container, users);
+
+    const adminLabels = Array.from(container.querySelectorAll('li')).filter((li) => li.textContent?.includes('(Admin)'));
+    expect(adminLabels).toHaveLength(adminUsers.length);
+  });
+
+  test('should not render admin label for regular users', () => {
+    localStorage.setItem('userRole', 'admin');
+
+    const container = document.createElement('div');
+    renderItems(container, users);
+
+    const userItems = Array.from(container.querySelectorAll('li'));
+    const nonAdminLabels = userItems.filter((li) => !li.textContent?.includes('(Admin)'));
+
+    expect(nonAdminLabels).toHaveLength(nonAdminUsers.length);
+  });
+
+  test('should render name and age for all users', () => {
+    localStorage.setItem('userRole', 'admin');
+
+    const container = document.createElement('div');
+    renderItems(container, users);
+
+    const userItems = Array.from(container.querySelectorAll('li'));
+
+    userItems.forEach((userItem, index) => {
+      expect(userItem.textContent).toContain(`Name: ${users[index].name}, Age: ${users[index].age}`);
+    });
   });
 });
