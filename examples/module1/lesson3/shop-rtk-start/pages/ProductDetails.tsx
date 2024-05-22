4d7413dd402772cb9e3f768ebe10aa9d00ee39de
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-05-22 09:34:52 +0200

    module1-lesson3-task1: rtk refactor

--- examples/module1/lesson3/shop-rtk-start/pages/ProductDetails.tsx
+++ examples/module1/lesson3/shop-rtk-start/pages/ProductDetails.tsx
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { CartContext } from '../contexts/CartContext';
-import { ProductContext } from '../contexts/ProductContext';
+import {useAppDispatch} from '../hooks/rtk';
+import {addToCart} from '../state/cartSlice';
+import { useGetProductsQuery } from '../services/product';
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { addToCart } = useContext(CartContext);
-  const { products } = useContext(ProductContext);
+  const { data: products} = useGetProductsQuery();
+  const dispatch = useAppDispatch();
+
+  if (!products) return <div className="text-center">Loading...</div>;
 
   const product = products.find((item) => {
     return item.id === parseInt(id!);
@@ -37,7 +39,7 @@ const ProductDetails = () => {
             </div>
             <p className="mb-8">{description}</p>
             <button
-              onClick={() => addToCart(product)}
+              onClick={() => dispatch(addToCart(product))}
               className="bg-green-600 py-4 px-8 text-white"
             >
               Add to cart
--- examples/module1/lesson3/shop-rtk-start/store.ts
+++ examples/module1/lesson3/shop-rtk-start/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import cartReducer from './state/cartSlice';
+import {productApi} from './services/product'
 
 export const store = configureStore({
   reducer: {
+    [productApi.reducerPath]: productApi.reducer,
     cart: cartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
