2638c4b3fa868520e3420ff72d127f2bca0461e2
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-05-27 19:30:20 +0200

    module1-lesson4-task2: measure request time

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: Date.now()}
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const elapsedTime = Date.now() - response.config.metadata.startTime;
+
+  console.log('Duration (ms):', elapsedTime);
   return response;
 });
 
